692f9cd5fd72d80f52ef9d712f20f6a2169ae9f5
Ako Shergazy <akylaiyms@bennington.edu>
2021-06-05 14:12:35 +0300

    update

--- usdan_withbuttons/app-files/data.js
+++ usdan_withbuttons/app-files/data.js
@@ -53,7 +53,7 @@ var APP_DATA = {
         {
           "yaw": 1.175427044973226,
           "pitch": 0.019852284185255087,
-          "title": "A",
+          "title": "Alex Parrasch",
           "text": "<a href=\"https://seniorshow2021.bennington.edu/portfolio/alex-parrasch/\" target=\"_blank\">see portfolio</a>\n<img src=\"photos/alex_parrasch.jpeg\" width=\"500\" height=\"auto\">"
         },
         {
